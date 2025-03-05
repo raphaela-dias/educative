56dc4b4bc169f0c208034ed3ef35b0f3c0fe55a3
Raphaela Dias <raphaelalmd@gmail.com>
2025-03-05 14:02:11 -0500

    Math test project finished

--- Become a Java Developer/Module 1 - Learn Java/math-test/src/Main.java
+++ Become a Java Developer/Module 1 - Learn Java/math-test/src/Main.java
@@ -7,6 +7,30 @@ public class Main {
         System.out.println("Hello, %s! Welcome to the math quiz.\nYou will be provided with 5 questions, and each carries one mark. Good luck!".formatted(userName));
     }
 
+    public static String chooseOperator(int opNum){
+        if (opNum == 1) {
+            return "-";
+        }
+        else if (opNum == 2){
+            return "+";
+        }
+        else if (opNum == 3){
+            return "*";
+        }
+        else{
+            return "/";
+        }
+    }
+
+    public static int calculateAnswer(int n1, int n2, String op){
+        return switch (op) {
+            case "-" -> n1 - n2;
+            case "+" -> n1 + n2;
+            case "*" -> n1 * n2;
+            default -> (int) (n1 / n2);
+        };
+    }
+
     public static void printQuestion(int num1, int num2, String op){
         System.out.println("What is %d %s %d?".formatted(num1, op, num2));
     }
@@ -29,45 +53,28 @@ public class Main {
 
         Scanner scan = new Scanner(System.in);
         Random random = new Random();
-        int num1, num2, userAnswer, correctAnswer, score = 0;
-        String userName, op, question, farewellMessage;
+        int num1 = 0, num2 = 0, userAnswer = 0, correctAnswer = 0, opNum = 0, score = 0;
+        String userName, op = "";
 
         System.out.print("Please enter your name: ");
         userName = scan.nextLine();
 
         greetUser(userName);
 
-        num1 = random.nextInt(10) + 1;
-        num2 = random.nextInt(10) + 1;
-        op = "-";
-        printQuestion(num1, num2, op);
-        userAnswer = scan.nextInt();
-        correctAnswer = num1 - num2;
-        score = answerFeedback(userAnswer, correctAnswer, score);
+        for (int i = 0; i < 5; i++) {
 
-        num1 = random.nextInt(10) + 1;
-        num2 = random.nextInt(10) + 1;
-        op = "+";
-        printQuestion(num1, num2, op);
-        userAnswer = scan.nextInt();
-        correctAnswer = num1 + num2;
-        score = answerFeedback(userAnswer, correctAnswer, score);
+            num1 = random.nextInt(10) + 1;
+            num2 = random.nextInt(10) + 1;
+            opNum = random.nextInt(4) + 1;
+            op = chooseOperator(opNum);
+            printQuestion(num1, num2, op);
+            userAnswer = scan.nextInt();
+            correctAnswer = calculateAnswer(num1, num2, op);
+            System.out.println("Your answer is: " + userAnswer);
+            System.out.println("The correct answer is: " + correctAnswer);
+            score = answerFeedback(userAnswer, correctAnswer, score);
 
-        num1 = random.nextInt(10) + 1;
-        num2 = random.nextInt(10) + 1;
-        op = "*";
-        printQuestion(num1, num2, op);
-        userAnswer = scan.nextInt();
-        correctAnswer = num1 * num2;
-        score = answerFeedback(userAnswer, correctAnswer, score);
-
-        num1 = random.nextInt(10) + 1;
-        num2 = random.nextInt(10) + 1;
-        op = "/";
-        printQuestion(num1, num2, op);
-        userAnswer = scan.nextInt();
-        correctAnswer = num1 / num2;
-        score = answerFeedback(userAnswer, correctAnswer, score);
+        }
 
         printResult(userName, score);
 
